ab1b76694d9db7649158c87b1511d15f376b237b
Asmer <asmeryuan@163.com>
2020-08-24 00:22:04 +0800

    上下文关联学习

---
+++ common/javacript系列之跟着underscore学习/改变上下文/es6.js
@@ -0,0 +1,21 @@
+function vararea() {
+  var a = [];
+  for (var i = 0; i < 10; i++) {
+    a[i] = function () {
+      console.log(i);
+    };
+  }
+  a[6]();
+}
+vararea();
+
+function letarea() {
+  let a = [];
+  for (let i = 0; i < 10; i++) {
+    a[i] = function () {
+      console.log(i);
+    };
+  }
+  a[6]();
+}
+letarea();
