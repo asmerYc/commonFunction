1ff05ff21a0a6299d225521039075e44a6486a96
Asmer <asmeryuan@163.com>
2020-10-11 11:32:00 +0800

    moment学习

--- common/moment/moment.js
+++ common/moment/moment.js
@@ -5,18 +5,18 @@
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /commonFunction/moment/moment.js
- */ 
-var moment = require('moment')
-var now = moment()
+ */
+var moment = require("moment");
+var now = moment();
 var day = moment("1995-12-25");
 // console.log(moment().day())
-const string = "2020-07-10 20:10:00"
+const string = "2020-07-10 20:10:00";
 // console.log(string.split(" ")[0])
-// console.log(moment(string).format('YYYY-MM-DD'))
+// console.log(moment(string).format("YYYY-MM-DD"));
+console.log(moment(string).format("YYYY-MM-DD"));
 
-
-console.log(new Date())
-console.log(moment())
-console.log(moment(new Date()))
-console.log(moment(string))
-console.log(moment('1995-12-14'))
+console.log(new Date());
+console.log(moment());
+console.log(moment(new Date()));
+console.log(moment(string));
+console.log(moment("1995-12-14"));
